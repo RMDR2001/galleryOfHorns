e2034c6ca7926e969e1976a409ea98405746e7f4
Ricardo_Mauricio_Delgado_Rivera <rmdr2001@gmail.com>
2024-08-22 20:43:17 -0500

    Lab02

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,16 +1,26 @@
 import HornedBeast from './HornedBeast';
-import hornedBeast from './hornedBeast.json';
+import hornedBeastLab2 from './hornedBeastLab2.json'
+import { Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
 
-function Gallery(){
+function Gallery() {
     return (
         <>
-        <HornedBeast title={"imagen1"} description={"Esta es una imagen de referencia"} imagenURL={"https://plus.unsplash.com/premium_photo-1661810799921-ded27d475104?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
-        <HornedBeast title={"toro"} description={"Esta es una imagen de referencial de un toro"} imagenURL={"https://images.unsplash.com/photo-1532784774615-647af4990186?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}/>
-        {
-            hornedBeast.map(data => {
-                <HornedBeast title={data.title} description={data.description} imagenURL={data.image_url}/>
-            })
-        }
+            <Container>
+                <Row xs={2} sm={3} md={4} lg={5}>
+                    {
+                        hornedBeastLab2.map((data, index) =>
+                            <HornedBeast
+                                key={index}
+                                title={data.title}
+                                description={data.description}
+                                imagenURL={data.image_url}
+                                alt={data.keyword}
+                                horns={data.horns} />)
+                    }
+                </Row>
+            </Container>
+
         </>
     );
 }
--- src/HornedBeast.jsx
+++ src/HornedBeast.jsx
@@ -1,9 +1,28 @@
+import React, { useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
 function HornedBeast(props) {
+
+    const [favorites, setFavorites] = useState(0);
+
+    const handleFavoriteClick = () => setFavorites(favorites+1);
+
     return(
         <>
-        <h2>{props.title}</h2>
-        <img src={props.imagenURL} style={{width:'200px'}} alt={props.title}/>
-        <p>{props.description}</p>
+            {/* <h2>{props.title}</h2>
+            <img src={props.imagenURL} style={{width:'200px'}} alt={props.alt}/>
+            <p>{props.description}</p>
+            <p>Horns: {props.horns}</p>
+            <button onClick={handleFavoriteClick} style = {{fontSize: '16px', padding: '5px 5px', cursor: 'pointer'}}>❤️: {favorites}</button> */}
+
+            <Card style={{ width: '18rem', marginBottom: '10px'}}>
+                <Card.Img variant="top" src={props.imagenURL} />
+                <Card.Body>
+                    <Card.Title>{props.title}</Card.Title>
+                    <Card.Text>{props.description}</Card.Text>
+                    <Button variant="primary"onClick={handleFavoriteClick} style = {{fontSize: '16px', padding: '5px 5px', cursor: 'pointer'}}>❤️: {favorites}</Button>
+                </Card.Body>
+            </Card>
         </>
     );
 }
